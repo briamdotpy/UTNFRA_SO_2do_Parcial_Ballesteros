972c34c619f42efb4a86caa6a001a18aa33637bd
briamdotpy <ballesterosbrian03@gmail.com>
2024-11-12 23:11:41 +0000

    ADD:intentando hacer el punto B.

---
+++ RTA_Examen_20241110/Punto_B.sh
@@ -0,0 +1,4 @@
+"Bash Scripting Fallido.
+intente hacer el punto B de la manera en la cual usted hacia en la clase pero no logro ejecutarlo, intente con comandos que ejecutaron otros compañeros pero tampoco tuvo solución.
+Punto B: 
+Creamos las variables, obtenemos las contraseñas, recorremos el archivo, si el grupo no existe lo creamos y lo mismo con los directorios. Luego agregamos al usuario con su grupo y directorio. Al final restauramos el valor de IFS."
