3c8f77b16bf15a2a2485ce38822f169fa480f479
briamdotpy <ballesterosbrian03@gmail.com>
2024-11-13 00:39:16 +0000

    fix: Reparando el punto B, esta vez bien realizado.

--- RTA_Examen_20241110/Punto_B.sh
+++ RTA_Examen_20241110/Punto_B.sh
@@ -4,4 +4,6 @@ Punto B:
 Creamos las variables, obtenemos las contraseñas, recorremos el archivo, si el grupo no existe lo creamos y lo mismo con los directorios. Luego agregamos al usuario con su grupo y directorio. Al final restauramos el valor de IFS."
 
 
-"Modifique los comandos que estaba ejecutando para que funcione esta vez y si se crearon los usuarios correspondientes." 
+"Modifique los comandos que estaba ejecutando para que funcione esta vez y si se crearon los usuarios correspondientes."
+"Volvi a dar permisos: chmod +x"
+"sudo /usr/local/bin/BallesterosAltauser-Groups.sh Ballesteros /home/Ballesteros/UTNFRA_SO_2do_Parcial_Ballesteros/202406/bash_script/Lista_Usuarios.txt" 
