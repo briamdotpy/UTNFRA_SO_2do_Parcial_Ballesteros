ae28a59188c629205e70b15de25934afaa72164a
briamdotpy <ballesterosbrian03@gmail.com>
2024-11-13 00:03:09 +0000

    fix: Reparando el punto B, esta vez bien realizado.

--- RTA_Examen_20241110/Punto_B.sh
+++ RTA_Examen_20241110/Punto_B.sh
@@ -2,3 +2,6 @@
 intente hacer el punto B de la manera en la cual usted hacia en la clase pero no logro ejecutarlo, intente con comandos que ejecutaron otros compañeros pero tampoco tuvo solución.
 Punto B: 
 Creamos las variables, obtenemos las contraseñas, recorremos el archivo, si el grupo no existe lo creamos y lo mismo con los directorios. Luego agregamos al usuario con su grupo y directorio. Al final restauramos el valor de IFS."
+
+
+"Modifique los comandos que estaba ejecutando para que funcione esta vez y si se crearon los usuarios correspondientes." 
